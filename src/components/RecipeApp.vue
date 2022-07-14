418f6163f9fe0f360d4bf671ea99d9e7117d4586
Callum-OP <callumoprey@gmail.com>
2022-07-14 12:01:10 +0100

    Added and displayed array for recipes

--- src/components/RecipeApp.vue
+++ src/components/RecipeApp.vue
@@ -4,12 +4,24 @@
 
     <div id="addRecipe">
       <p>Add a new recipe</p>
-      <input type="text" id="recipe">
-      <input type="submit" value="Add">
+      <input :value="text" placeholder="Enter title here">
+      <br><br>
+      <input :value="text" placeholder="Enter ingredients here">
+      <br><br>
+      <div>
+        <button> Add </button>
+      </div>
     </div>
 
     <div id="showRecipes">
-
+    <p>View recipes</p>
+    <ul>
+      <li v-for="recipe in recipes" :key="recipe.title">
+        {{ recipe.title }} :
+        {{ recipe.ingredients }}
+        <br><br>
+      </li>
+    </ul>
     </div>
 
   </div>
@@ -20,6 +32,19 @@ export default {
   name: 'RecipeApp',
   props: {
     msg: String
+  },
+  data() {
+    return {
+      recipes: [
+        { title: 'Banana Milkshake', ingredients: '2 bananas and 1 cup of milk' },
+        { title: 'Ham Sandwich', ingredients: '1 slice of bread and 1 slice of ham' }
+      ],
+    }
+  },
+  methods: {
+    add(){
+      
+    }
   }
 }
 </script>
